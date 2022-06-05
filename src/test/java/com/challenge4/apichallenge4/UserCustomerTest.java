c90f8cb07420c8f251dd7933783ece1f97291184
hendra <inibuatapasih@gmail.com>
2022-06-05 23:50:39 +0700

    fixing conflict from customize url login branch

--- src/test/java/com/challenge4/apichallenge4/UserCustomerTest.java
+++ src/test/java/com/challenge4/apichallenge4/UserCustomerTest.java
@@ -1,36 +1,4 @@
-<<<<<<< HEAD
-package com.challenge4.apichallenge4;
 
-import com.challenge4.apichallenge4.Repository.UsersRepo;
-import com.challenge4.apichallenge4.Service.UsersService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-class UserCustomerTest {
-
-    @Mock private UsersRepo usersRepo;
-
-    private UsersService usersService;
-
-    @BeforeEach void setUp()
-    {
-        this.usersService
-                = new UsersService(this.usersRepo);
-    }
-
-    @Test void getAllPerson()
-    {
-        usersService.list_users();
-        Mockito.verify(usersRepo).findAll();
-    }
-
-}
-=======
 //package com.challenge4.apichallenge4;
 //
 //import com.challenge4.apichallenge4.Repository.UsersRepo;
@@ -61,4 +29,3 @@ class UserCustomerTest {
 //        Mockito.verify(usersRepo).findAll();
 //    }
 //}
->>>>>>> customize-url-login
--- src/test/java/com/challenge4/apichallenge4/UserRepositoryTest.java
+++ src/test/java/com/challenge4/apichallenge4/UserRepositoryTest.java
@@ -1,48 +1,4 @@
-<<<<<<< HEAD
-package com.challenge4.apichallenge4;
 
-
-import com.challenge4.apichallenge4.Entity.Users;
-import com.challenge4.apichallenge4.Repository.UsersRepo;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = ApiChallenge4ApplicationTests.class)
-public class UserRepositoryTest {
-    @Autowired
-    UsersRepo usersRepo;
-
-    @Test
-    public void testFindById() {
-        Users user = getCustomer();
-        Users userFound = usersRepo.findById(user.getId_users());
-        assertEquals(user.getId_users(), userFound.getId_users());
-    }
-
-    @Test
-    public void testSave() {
-        Users customer = getCustomer();
-        usersRepo.save(customer);
-        Users found = usersRepo.findById(customer.getId_users());
-        assertEquals(customer.getId_users(), found.getId_users());
-    }
-
-    private Users getCustomer() {
-        Users users = new Users();
-        users.setId_users(1);
-        users.setUsername("test");
-        users.setPassword("test");
-        users.setEmail("test@test.com");
-
-        return users;
-    }
-}
-=======
 //package com.challenge4.apichallenge4;
 //
 //
@@ -86,4 +42,3 @@ public class UserRepositoryTest {
 //        return users;
 //    }
 //}
->>>>>>> customize-url-login
